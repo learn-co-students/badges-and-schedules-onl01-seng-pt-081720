f277734c62d16e66c6c72d314e1801377fdb261e
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-21 01:50:20 +0000

    Done.

--- LICENSE.md
+++ LICENSE.md
@@ -1,4 +1,4 @@
-#Learn.co Educational Content License
+e#Learn.co Educational Content License
 
 Copyright (c) 2015 Flatiron School, Inc
 
--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(attendees)
+  badge = []
+  attendees.each do |name|
+    badge.push("Hello, my name is #{name}.")
+end
+return badge
+end
+
+
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do|name, index|
+   room_assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end
+  return room_assignments
+end
+
+
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |hello|
+    puts hello
+  end
+    assign_rooms(attendees).each do |rooms|
+  puts rooms
+end
+end
