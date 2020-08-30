a23c2b34f78248e64f5dc01854b72ea330df3169
Myles Janik <mylesjanik@gmail.com>
2020-08-30 04:20:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+list_of_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  new_array = []
+  array_of_names.each do |name|
+    puts "Hello, my name is #{name}."
+    new_array.push(name)
+  end
+  return new_array
+end
+
+#def assign_rooms(array_of_speakers)
+  new_array = []
+  counter = 1
+  array_of_speakers.each do |name|
+    new_array.push("Hello, #{name}! You'll by assigned to room #{counter}!")
+    counter++
+  end
+  return new_array
+end
+
+#def printer
+#  batch_badge_creator(list_of_speakers)
+#end
