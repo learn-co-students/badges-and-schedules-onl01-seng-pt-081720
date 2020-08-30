248b827cd226b1dd956b0ca717c987a503424f90
Myles Janik <mylesjanik@gmail.com>
2020-08-30 04:22:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,7 @@ end
 def batch_badge_creator(array_of_names)
   new_array = []
   array_of_names.each do |name|
-    puts "Hello, my name is #{name}."
-    new_array.push(name)
+    new_array.push("Hello, my name is #{name}.")
   end
   return new_array
 end
