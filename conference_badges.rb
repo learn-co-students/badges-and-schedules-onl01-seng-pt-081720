97d31081d337d5794daff32a8557f360ae12ebf6
Myles Janik <mylesjanik@gmail.com>
2020-08-30 04:20:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,14 +16,14 @@ def batch_badge_creator(array_of_names)
 end
 
 #def assign_rooms(array_of_speakers)
-  new_array = []
-  counter = 1
-  array_of_speakers.each do |name|
-    new_array.push("Hello, #{name}! You'll by assigned to room #{counter}!")
-    counter++
-  end
-  return new_array
-end
+#  new_array = []
+#  counter = 1
+#  array_of_speakers.each do |name|
+#    new_array.push("Hello, #{name}! You'll by assigned to room #{counter}!")
+#    counter++
+#  end
+#  return new_array
+#end
 
 #def printer
 #  batch_badge_creator(list_of_speakers)
