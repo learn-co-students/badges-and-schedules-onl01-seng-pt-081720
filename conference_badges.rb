0a2b77d680d41671cf21aaf56bbe89d7196c99c9
Christian Pardo <chrispardopersonal@gmail.com>
2020-08-31 04:41:50 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,12 @@ def assign_rooms(attendees)
   end 
 end 
 
-def printer()
-
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+  puts badge
+  end
+  assign_rooms(attendees).each do |assigned_room|
+    puts assigned_room
+    end
 end
+
